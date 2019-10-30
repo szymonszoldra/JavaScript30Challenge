e437a8a05dc9f0af93914e3a93cb2950fa2afd49
szymonszoldra <szymon.szoldra@gmail.com>
2019-10-30 19:36:51 +0100

    add edits in style.css and soultion for day 02

---
+++ 02 - JS and CSS clock/main.js
@@ -0,0 +1,17 @@
+const hands = {
+	second: document.querySelector(".second-hand"),
+	minute: document.querySelector(".min-hand"),
+	hour: document.querySelector(".hour-hand"),
+};
+
+setInterval(() => {
+	const time = new Date();
+	const currentTime = {
+		second: time.getSeconds(),
+		minute: time.getMinutes(),
+		hour: time.getHours(),
+	}
+	hands.second.style.transform = `rotate(${90 + currentTime.second*6}deg)`;
+	hands.minute.style.transform = `rotate(${90 + currentTime.minute*6}deg)`;
+	hands.hour.style.transform = `rotate(${90 + currentTime.hour*30}deg)`;
+}, 1)
