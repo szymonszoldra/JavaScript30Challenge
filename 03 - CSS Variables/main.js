1468cdc66123e08eae4401e0a1bd162728970c89
szymonszoldra <szymon.szoldra@gmail.com>
2019-10-31 11:45:26 +0100

    add solution to day 03

---
+++ 03 - CSS Variables/main.js
@@ -0,0 +1,9 @@
+const inputs = [...document.querySelectorAll('.controls input')];
+const root = document.documentElement;
+
+function changeStyle(e) {
+	if (e.target.name === "base") return root.style.setProperty(`--${e.target.name}`, `${e.target.value}`);
+	root.style.setProperty(`--${e.target.name}`, `${e.target.value}px`);
+}
+
+inputs.forEach(input => input.addEventListener('input', (e) => changeStyle(e)));
