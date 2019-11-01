fe6a4cddff9e597d67ac377de186acc8c76ead3c
szymonszoldra <szymon.szoldra@gmail.com>
2019-11-01 23:41:20 +0100

    add solution for day 04

--- 04 - Array Cardio Day 1/main.js
+++ 04 - Array Cardio Day 1/main.js
@@ -77,24 +77,67 @@ const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick
 // Array.prototype.filter()
 // 1. Filter the list of inventors for those who were born in the 1500's
 
+const firstAnswer = inventors.filter(inventor => Math.floor(inventor.year / 100) === 15);
+console.table(firstAnswer);
+
 // Array.prototype.map()
 // 2. Give us an array of the inventors' first and last names
 
+const secondAnswer = inventors.map(inventor => `${inventor.first} ${inventor.last}`);
+console.table(secondAnswer);
+
 // Array.prototype.sort()
 // 3. Sort the inventors by birthdate, oldest to youngest
+const thirdAnswer = inventors.sort((a, b) => a.year > b.year ? 1 : -1);
+
+
+console.table(thirdAnswer);
 
 // Array.prototype.reduce()
 // 4. How many years did all the inventors live?
 
+const fourthAnswer = inventors.reduce((acc, inventor) => acc += (inventor.passed - inventor.year), 0);
+console.log(`All inventors lived ${fourthAnswer} years`);
+
 // 5. Sort the inventors by years lived
+
+const fifthAnswer = inventors.sort((a, b) => {
+	const aYearsLived = a.passed - a.year;
+	const bYearsLived = b.passed - b.year;
+	return aYearsLived > bYearsLived ? -1 : 1;
+})
+console.table(fifthAnswer);
 
 // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
 // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
 
+const boulevards = [...document.querySelectorAll('.mw-category a')];
+
+//READY CODE BELOW, COPY THAT
+// const sixthAnswer = boulevards
+// 	.filter(boulevard => boulevard.textContent.includes('de'))
+// 	.map(boulevard => boulevard.textContent);
+// console.table(sixthAnswer);
+
 
 // 7. sort Exercise
 // Sort the people alphabetically by last name
+const seventhAnswer = people.sort((a, b) => {
+	const [aFirstName, aLastName] = a.split(', ');
+	const [bFirstName, bLastName] = b.split(', ');
+	return aLastName > bLastName ? 1 : -1;
+});
+console.table(seventhAnswer);
+
 
 // 8. Reduce Exercise
 // Sum up the instances of each of these
 const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck'];
+
+const eighthAnswer = data.reduce((acc, instance) => {
+	if (!acc[instance]) acc[instance] = 0;
+	acc[instance]++;
+	return acc;
+}, {})
+
+console.table(eighthAnswer);
