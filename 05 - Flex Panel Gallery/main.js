1e172d88854c713dcbcd574451de5ef711031c1d
szymonszoldra <szymon.szoldra@gmail.com>
2019-11-02 13:14:12 +0100

    add changes in parapgraph motion in js, change classes styles in css

--- 05 - Flex Panel Gallery/main.js
+++ 05 - Flex Panel Gallery/main.js
@@ -1,7 +1,7 @@
 const containter = document.querySelector('.panels');
 const panels = containter.querySelectorAll('.panel');
 
-const handleRemove = () => panels.forEach(panel => panel.classList.remove('active'));
+const handleRemove = () => panels.forEach(panel => panel.classList.remove('active', 'fullyActive'));
 
 const handleClick = (e) => {
 	const isContaining = e.target.classList.contains('active') || e.target.parentNode.classList.contains('active');
@@ -9,5 +9,13 @@ const handleClick = (e) => {
 	handleRemove();
 	e.target.nodeName === "DIV" ? e.target.classList.add('active') : e.target.parentNode.classList.add('active');
 }
-
+const handleParagraphsVisibility = (e) => {
+	const name = e.propertyName === "flex" || e.propertyName === "flex-grow";
+	const doesItHaveActiveClass = e.target.classList.contains('active') || e.target.parentNode.classList.contains('active');
+	if (name && doesItHaveActiveClass) {
+		const className = 'fullyActive';
+		e.target.nodeName === "DIV" ? e.target.classList.add(className) : e.target.parentNode.classList.add(className);
+	}
+}
 panels.forEach(panel => panel.addEventListener('click', e => handleClick(e)));
+panels.forEach(panel => panel.addEventListener('transitionend', e => handleParagraphsVisibility(e)));
