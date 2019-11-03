dd5faf746ad1ecdbf0b6b1cc3534e9b4851c5c6b
szymonszoldra <szymon.szoldra@gmail.com>
2019-11-03 21:52:22 +0100

    add solution for day 06

--- 06 - Type Ahead/main.js
+++ 06 - Type Ahead/main.js
@@ -1,1 +1,29 @@
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+
+const input = document.querySelector('.search');
+const ul = document.querySelector('ul');
+const citiesData = [];
+
+fetch(endpoint)
+	.then(res => res.json())
+	.then(res => citiesData.push(...res))
+	.catch(err => {
+		throw new Error(err)
+	})
+
+const handleChange = (exp, cities) => {
+	const value = exp.toLowerCase();
+	if (cities.length) {
+		const matchingCities = cities.filter(city => city.city.toLowerCase().includes(value) || city.state.toLowerCase().includes(value));
+		ul.innerHTML = '';
+		matchingCities.forEach(city => {
+			const li = document.createElement('li');
+			li.innerHTML = `<span class="name">${city.city}, ${city.state}</span> <span class="population">${city.population}</span>`;
+			ul.appendChild(li);
+		})
+	} else {
+		alert("Wait for cities to load");
+		return setTimeout(handleChange(input.value, cities), 3000)
+	}
+}
+input.addEventListener('input', e => handleChange(e.target.value, citiesData));
