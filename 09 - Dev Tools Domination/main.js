f81f52b8350fd3b4e9fd501435a86bb6bd93a6c4
szymonszoldra <szymon.szoldra@gmail.com>
2019-11-06 16:17:26 +0100

    add initial files for day 09

---
+++ 09 - Dev Tools Domination/main.js
@@ -0,0 +1,31 @@
+const dogs = [{
+	name: 'Snickers',
+	age: 2
+}, {
+	name: 'hugo',
+	age: 8
+}];
+
+// Regular
+
+// Interpolated
+
+// Styled
+
+// warning!
+
+// Error :|
+
+// Info
+
+// Testing
+
+// clearing
+
+// Viewing DOM Elements
+
+// Grouping together
+
+// counting
+
+// timing
