93267778a8e76decab37ff1ba8d7dd7df7b3983a
szymonszoldra <szymon.szoldra@gmail.com>
2019-11-06 16:27:38 +0100

    add solution for day 09

--- 09 - Dev Tools Domination/main.js
+++ 09 - Dev Tools Domination/main.js
@@ -7,25 +7,57 @@ const dogs = [{
 }];
 
 // Regular
+console.log('Hello world!')
 
 // Interpolated
+console.log('Hello %s', 'world!');
 
 // Styled
+console.log('%cHello world!', 'color:royalblue;');
 
 // warning!
+console.warn('Hello world!')
 
 // Error :|
+console.error('Hello world!')
 
 // Info
+console.info('Hello world!')
 
 // Testing
+const dog = 'cat';
+console.assert(dog === 'dog', 'Hello world!')
 
 // clearing
+console.clear();
 
 // Viewing DOM Elements
+console.log(document.querySelector('p'));
 
 // Grouping together
+console.groupCollapsed('Hello world');
+console.log('Hello');
+console.log('world!');
+console.groupEnd('Hello world!');
 
 // counting
+console.count('Hello');
+console.count('Hello');
+console.count('Hello');
+console.count('world!');
+console.count('world!');
+console.count('world!');
 
 // timing
+
+console.time('Fetching data')
+fetch('https://api.github.com/users/szymonszoldra')
+	.then(data => data.json())
+	.then(data => {
+		console.timeEnd('Fetching data');
+		console.log(data);
+	})
+
+// table
+
+console.table(dogs);
