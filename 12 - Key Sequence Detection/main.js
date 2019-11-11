0e082144c218f694d1630bbe84edb541b5128838
szymonszoldra <szymon.szoldra@gmail.com>
2019-11-11 22:02:24 +0100

    add solution for day 12

---
+++ 12 - Key Sequence Detection/main.js
@@ -0,0 +1,15 @@
+const keysPressed = [];
+const code = 'szymonszoldra'
+
+const handleSecretCode = e => {
+	keysPressed.push(e.key);
+	keysPressed.length > code.length ? keysPressed.shift() : null;
+
+	const keys = keysPressed.join('');
+	const isCodeHere = keys.indexOf(code);
+
+	isCodeHere != -1 ? cornify_add() : null;
+	return;
+}
+
+window.addEventListener('keypress', e => handleSecretCode(e));
