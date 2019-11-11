6fae0ed26423e0c1009aa326b66fbdcb7d97d5cf
szymonszoldra <szymon.szoldra@gmail.com>
2019-11-11 23:22:13 +0100

    add solution for day 13

--- 13 - Slide in on Scroll/main.js
+++ 13 - Slide in on Scroll/main.js
@@ -13,3 +13,24 @@ function debounce(func, wait = 20, immediate = true) {
 		if (callNow) func.apply(context, args);
 	};
 }
+
+const images = document.querySelectorAll('.slide-in');
+
+const handleScroll = e => {
+	images.forEach(image => {
+		const slideInAt = (window.scrollY + window.innerHeight) - image.height / 2;
+
+		const imageBottomEdge = image.offsetTop + image.height;
+
+		const isHalfShown = slideInAt > image.offsetTop;
+		const isNotScrolledPast = window.scrollY < imageBottomEdge;
+
+		if (isHalfShown && isNotScrolledPast) {
+			image.classList.add('active')
+		} else {
+			image.classList.remove('active')
+		}
+	})
+}
+
+window.addEventListener('scroll', debounce(handleScroll))
