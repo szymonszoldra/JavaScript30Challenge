bade7dc1a68c06831f606c27823f49de68960bb7
szymonszoldra <szymon.szoldra@gmail.com>
2019-11-14 23:22:48 +0100

    add solution for day 17

--- 17 - Sort Without Articles/main.js
+++ 17 - Sort Without Articles/main.js
@@ -1,1 +1,11 @@
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+
+const removeArticles = bands => bands.replace(/^(a |an |the)/i, '').trim();
+const sortedBands = bands.sort((a, b) => removeArticles(a) > removeArticles(b) ? 1 : -1);
+
+bands.forEach(band => {
+	const li = document.createElement('li');
+	li.textContent = band;
+	document.querySelector('#bands').appendChild(li);
+})
